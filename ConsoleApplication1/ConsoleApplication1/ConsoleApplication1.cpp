a9c1ba7b09b53ccaf3d7428d4f48a5c2d2a5e8d5
Vilen0 <125581689+Vilen0@users.noreply.github.com>
2024-05-15 11:14:03 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <thread>
+using namespace std;
+
+void print() {
+    for (int i = 0; i < 10; ++i) {
+         cout << "Дочерний поток: строка " << i + 1 <<  endl;
+    }
+} 
+
+int main() {
+    setlocale(LC_ALL, "Ru");
+
+    thread childThread(print);
+    childThread.join();
+    for (int i = 0; i < 10; ++i) {
+         cout << "Родительский поток: строка " << i + 1 <<  endl;
+    }
+
+    return 0;
+}
