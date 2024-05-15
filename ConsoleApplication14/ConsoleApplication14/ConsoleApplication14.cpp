10ee0f73b453b571542c73deef4fcb3c8b3b1979
Vilen0 <125581689+Vilen0@users.noreply.github.com>
2024-05-15 11:32:23 +0300

    Add files via upload

---
+++ ConsoleApplication14/ConsoleApplication14/ConsoleApplication14.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <atomic>
+using namespace std;
+
+mutex mu;
+atomic<int> parentSemaphore(1); 
+atomic<int> childSemaphore(0);  
+
+void print(const string& text, atomic<int>& semaphore1, atomic<int>& semaphore2) {
+    while (true) {
+        unique_lock<mutex> lock(mu);
+        if (semaphore1) {
+            cout << text << endl;
+            semaphore1 = 0;
+            semaphore2 = 1;
+            break;
+        }
+    }
+}
+
+void threadPrint() {
+    for (int i = 0; i < 10; ++i) {
+        print("Дочерний поток: пример...", childSemaphore, parentSemaphore);
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Ru");
+
+    thread childThread(threadPrint);
+
+    for (int i = 0; i < 10; ++i) {
+        print("Родительский поток: пример...", parentSemaphore, childSemaphore);
+    }
+
+    childThread.join();
+    return 0;
+}
---
+++ ConsoleApplication17/ConsoleApplication17/ConsoleApplication17.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <algorithm>
+using namespace std;
+
+struct Node {
+    string data;
+    Node* next;
+    Node(const string& data) : data(data), next(nullptr) {}
+};
+
+class LinkedList {
+private:
+    Node* head;
+    mutex mtx;
+
+public:
+    LinkedList() : head(nullptr) {}
+
+    void insert(const string& data) {
+        lock_guard<mutex> lock(mtx);
+        Node* newNode = new Node(data);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    void print() {
+        lock_guard<mutex> lock(mtx);
+        Node* current = head;
+        while (current != nullptr) {
+            cout << current->data << endl;
+            current = current->next;
+        }
+    }
+
+    void bubbleSort() {
+        lock_guard<mutex> lock(mtx);
+        if (!head) return;
+        bool swapped;
+        Node* ptr1;
+        Node* lptr = nullptr;
+        do {
+            swapped = false;
+            ptr1 = head;
+            while (ptr1->next != lptr) {
+                if (ptr1->data > ptr1->next->data) {
+                    swap(ptr1->data, ptr1->next->data);
+                    swapped = true;
+                }
+                ptr1 = ptr1->next;
+            }
+            lptr = ptr1;
+        } while (swapped);
+    }
+};
+
+void sortingThread(LinkedList& list) {
+    while (true) {
+        this_thread::sleep_for(chrono::seconds(5));
+        list.bubbleSort();
+    }
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+
+    LinkedList list;
+    thread sorting(sortingThread, ref(list));
+    while (true) {
+        string input;
+        getline(cin, input);
+        if (input.empty()) {
+            cout << "Текущее состояние списка:" << endl;
+            list.print();
+        } else {
+            if (input.length() > 80) {
+                for (size_t i = 0; i < input.length(); i += 80) {
+                    string substr = input.substr(i, 80);
+                    list.insert(substr);
+                }
+            } else {
+                list.insert(input);
+            }
+        }
+    }
+    sorting.join();
+    return 0;
+}
