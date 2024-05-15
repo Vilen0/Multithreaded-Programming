d241183b52bb1b6c30978c1ae995529ec9cb05c5
Vilen0 <125581689+Vilen0@users.noreply.github.com>
2024-05-15 11:29:41 +0300

    Add files via upload

---
+++ ConsoleApplication11/ConsoleApplication11/ConsoleApplication11.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
+using namespace std;
+
+mutex mu;
+
+void print() {
+    lock_guard<mutex> guard(mu);
+
+    cout << std::this_thread::get_id() << ": Print text..." << endl;
+
+}
+
+void threadPrint() {
+    for (int i = 0; i < 10; ++i) {
+        print();
+        chrono::milliseconds timeout(50);
+        this_thread::sleep_for(timeout);
+    }
+}
+
+
+
+int main() {
+    setlocale(LC_ALL, "Ru");
+
+    thread childThread(threadPrint);
+
+    threadPrint();
+
+    childThread.join();
+    return 0;
+}
---
+++ ConsoleApplication13/ConsoleApplication13/ConsoleApplication13.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+using namespace std;
+
+mutex mu;
+condition_variable cv;
+bool isChildTurn = false;
+
+void print(const string& text, bool isChild) {
+    unique_lock<mutex> lock(mu);
+    cv.wait(lock, [isChild] { return isChild == isChildTurn; }); 
+    cout << text << endl;
+    isChildTurn = !isChildTurn;
+    cv.notify_one(); 
+}
+
+void threadPrint() {
+    for (int i = 0; i < 10; ++i) {
+        print("Дочерний поток: пример...", true);
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Ru");
+
+    thread childThread(threadPrint);
+
+    for (int i = 0; i < 10; ++i) {
+        print("Родительский поток: пример...", false);
+    }
+
+    childThread.join();
+    return 0;
+}
