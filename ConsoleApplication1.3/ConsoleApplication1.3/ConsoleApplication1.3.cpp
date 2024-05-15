7106849cdbe3024af8ad602fbb66249b6f403e22
Vilen0 <125581689+Vilen0@users.noreply.github.com>
2024-05-15 11:16:12 +0300

    Add files via upload

---
+++ ConsoleApplication1.3/ConsoleApplication1.3/ConsoleApplication1.3.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <thread>
+#include <pthread.h>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+void printStrings(const vector<string>& strings) {
+    for (const auto& str : strings) {
+        cout << str << endl;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Ru");
+
+    vector<string> strings1 = { "Поток 1: Строка 1",
+        "Поток 1: Строка 2",
+        "Поток 1: Строка 3" };
+    vector<string> strings2 = { "Поток 2: Строка 1",
+        "Поток 2: Строка 2",
+        "Поток 2: Строка 3" };
+    vector<string> strings3 = { "Поток 3: Строка 1",
+        "Поток 3: Строка 2",
+        "Поток 3: Строка 3" };
+    vector<string> strings4 = { "Поток 4: Строка 1",
+        "Поток 4: Строка 2",
+        "Поток 4: Строка 3" };
+
+    thread thread1(printStrings, strings1);
+    thread thread2(printStrings, strings2);
+    thread thread3(printStrings, strings3);
+    thread thread4(printStrings, strings4);
+
+    thread1.join();
+    thread2.join();
+    thread3.join();
+    thread4.join();
+
+    return 0;
+}
---
+++ ConsoleApplication1.4/ConsoleApplication1.4/ConsoleApplication1.4.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <thread>
+#include <pthread.h>
+#include <chrono>
+using namespace std;
+
+void printExitMessage(void* arg) {
+	cout << "Поток завершается." << endl;
+}
+
+void* printStrings(void* parm) {
+	pthread_cleanup_push(printExitMessage, nullptr);
+	for (int i = 1; i < 7; i++) {
+		pthread_testcancel();
+		cout << "Строка " << i << endl;
+		
+		this_thread::sleep_for(chrono::milliseconds(300));
+	}
+	pthread_cleanup_pop(1);
+	return NULL;
+}
+
+int main() {
+	setlocale(LC_ALL, "Ru");
+
+	int rc = 0;
+	pthread_t thread1;
+	rc = pthread_create(&thread1, NULL, printStrings, NULL);
+	this_thread::sleep_for(chrono::milliseconds(2000));
+	rc = pthread_cancel(thread1);
+
+	pthread_exit(NULL);
+
+	return 0;
+}
